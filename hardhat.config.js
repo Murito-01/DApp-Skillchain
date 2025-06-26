8cc4315ebd67b0d0ea8a0b080540ee048a64cb00
Murito-01 <ristoo.abrar@gmail.com>
2025-06-26 20:11:06 +0700

    Configuring a local Hardhat network

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,5 +2,18 @@
 require("@nomicfoundation/hardhat-toolbox");
 
 module.exports = {
-  solidity: "0.8.28",
+  solidity: {
+    version: "0.8.28",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200  // Bisa disesuaikan, semakin tinggi semakin efisien gas tapi ukuran kontrak lebih besar
+      }
+    }
+  },
+  networks: {
+    hardhat: {
+      allowUnlimitedContractSize: true  // Untuk testing
+    }
+  }
 };
