fbcd37f9e201e2f8c670d37f9c1f6cc908007b4d
Murito-01 <ristoo.abrar@gmail.com>
2025-06-28 15:15:53 +0700

    Changing the json configuration

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,19 @@
 /** @type import('hardhat/config').HardhatUserConfig */
 require("@nomicfoundation/hardhat-toolbox");
 
+const fs = require("fs");
+const path = require("path");
+
+task("export-abi", "Export ABI ke frontend")
+  .setAction(() => {
+    const abi = require("./artifacts/contracts/MainContract.sol/MainContract.json").abi;
+    fs.writeFileSync(
+      path.join(__dirname, "client/src/abi/MainContract.json"),
+      JSON.stringify(abi, null, 2)
+    );
+    console.log("✅ ABI berhasil diexport ke client/src/abi/MainContract.json");
+  });
+
 module.exports = {
   solidity: {
     version: "0.8.28",
