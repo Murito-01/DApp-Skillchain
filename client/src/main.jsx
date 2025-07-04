e6a4db340ce0fa2fddcec4db484cf8d49047f5c5
Murito-01 <ristoo.abrar@gmail.com>
2025-07-04 13:52:39 +0700

    Update when refreshing the application will automatically go to the home page

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -8,3 +8,8 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
+
+// Tambahkan redirect ke home saat reload
+if (window.location.pathname !== '/') {
+  window.history.replaceState({}, '', '/');
+}
